5d864b594e2b470543e0cac3bf2d8c2f5a5b989a
lomopon <olegmakeev3@gmail.com>
2023-04-09 21:10:32 +0500

    Создание кнопки

---
+++ calculator/src/component/UI/Button/MyButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './MyButton.module.css'
+
+const MyButton = () => {
+    return(
+        <button className={classes.ButtonNumbers}>
+            
+        </button>
+    )
+}
+
+export default MyButton
