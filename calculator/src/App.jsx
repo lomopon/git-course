7ea948058ec2d55670f5cf25a87b7747b48b692b
lomopon <olegmakeev3@gmail.com>
2023-04-11 23:55:47 +0500

    button

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -3,18 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MyButton from './component/UI/Button/MyButton'
+import classes from './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const myValue = '2';
 
   return (
     <div className="App">
+      <div className="Btn"></div>
+      <div className="Btn"></div>
+      <div className="Btn"></div>
+      <div className="Btn">
       <MyButton myValue = '1' >1</MyButton>
       <MyButton myValue = '2' >2</MyButton>
       <MyButton myValue = '3' >3</MyButton>
       <MyButton myValue = '4' >4</MyButton>
+      </div>
+      <div className="Btn"></div>
     </div>
   )
 }
