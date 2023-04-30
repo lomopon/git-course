cde3bd0d5a1f751cf46e0bf3ce8c9579dcb1d3fb
lomopon <olegmakeev3@gmail.com>
2023-05-01 00:22:00 +0500

    доработка кнопок и высчитывание процентов

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,49 +7,86 @@ import classes from './component/UI/Button/MyButton.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const [text, setText] = useState('');
 
+  //массив заголовка 
+  const result = () => {
+    const tyextArra = text.split('');
+    console.log(tyextArra);
+    setText('0');
+  }
 
+  //очистка заголовка
+  const allClear = () => {
+    setText('0');
+  }
+  
+  //массив процента
+  const percent = () => {
+  const tyextArra2 = text;
+  const percent1 = tyextArra2 / 100;
+  console.log(percent1);
+  setText(percent1)
+  }
+
+  const handleClick = (symbol) => {
+    setText(text + symbol);
+  }
+
+  const handleBlur = (event) => {
+    // Обновляем состояние только при завершении ввода
+    setText(event.target.value);
+  };
+  
   return (
     <div className="App">
       <div>
-        <h1 className='Line'>Привет</h1>
+        <h1 className='Line'>{text}</h1>
       </div>
       <div>
         <div className="Btn">
-        <MyButton class={classes.ButtonNumbers} myValue = '7' myValuetwo ='' >7</MyButton>
-        <MyButton class={classes.ButtonNumbers} myValue = '7' myValuetwo ='' >7</MyButton>
-        <MyButton class={classes.SignsDivide} myValue = '/' myValuetwo ='' >/</MyButton>
-        <MyButton class={classes.SignsMultiply} myValue = '*' myValuetwo ='' >*</MyButton>
+          <MyButton percent={percent} class={classes.SignsPercent} >%</MyButton>
+          <MyButton class={classes.SignsBracket}>(</MyButton>
+          <MyButton class={classes.SignsBracket}>)</MyButton>
+        </div>
+      </div>
+      <div>
+        <div className="Btn">
+        <MyButton clear={allClear} class={classes.AllClear}>AC</MyButton>
+        <MyButton clear={allClear} class={classes.AllClear}>AC</MyButton>
+        <MyButton title= {handleClick} class={classes.SignsDivide} myValue = '/' myValuetwo ='' >/</MyButton>
+        <MyButton title= {handleClick} class={classes.SignsMultiply} myValue = '*' myValuetwo ='' >*</MyButton>
         </div>
       <div className="Btn" >
         <div>
           <div className="Btn">
-            <MyButton class={classes.ButtonNumbers} myValue = '7' myValuetwo ='' >7</MyButton>
-            <MyButton class={classes.ButtonNumbers} myValue = '8' myValuetwo ='' >8</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '7' myValuetwo ='' >7</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '8' myValuetwo ='' >8</MyButton>
           </div>
           <div className="Btn" >
-            <MyButton class={classes.ButtonNumbers} myValue = '4' myValuetwo ='' >4</MyButton>
-            <MyButton class={classes.ButtonNumbers} myValue = '5' myValuetwo ='' >5</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '4' myValuetwo ='' >4</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '5' myValuetwo ='' >5</MyButton>
           </div>
           <div className="Btn" >
-            <MyButton class={classes.ButtonNumbers} myValue = '1' myValuetwo ='' >1</MyButton>
-            <MyButton class={classes.ButtonNumbers} myValue = '2' myValuetwo ='' >2</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '1' myValuetwo ='' >1</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '2' myValuetwo ='' >2</MyButton>
           </div>
           <div>
-            <MyButton class={classes.zero} myValue = '0' myValuetwo ='' >0</MyButton>
+            <MyButton title= {handleClick} class={classes.zero} myValue = '0' myValuetwo ='' >0</MyButton>
           </div>
         </div>
         <div className="Btn">
           <div>
-            <MyButton class={classes.ButtonNumbers} myValue = '9' myValuetwo ='' >9</MyButton>
-            <MyButton class={classes.ButtonNumbers} myValue = '6' myValuetwo ='' >6</MyButton>
-            <MyButton class={classes.ButtonNumbers} myValue = '3' myValuetwo ='' >3</MyButton>
-            <MyButton class={classes.Dot} myValue = '.' myValuetwo ='' >.</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '9' myValuetwo ='' >9</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '6' myValuetwo ='' >6</MyButton>
+            <MyButton title= {handleClick} class={classes.ButtonNumbers} myValue = '3' myValuetwo ='' >3</MyButton>
+            <MyButton title= {handleClick} class={classes.Dot} myValue = ',' myValuetwo ='.' >.</MyButton>
           </div>
           <div>
-            <MyButton class={classes.Signs} myValue = '-' myValuetwo ='' >-</MyButton>
-            <MyButton class={classes.SignsPlus} myValue = '+' myValuetwo ='' >+</MyButton>
-            <MyButton class={classes.SignsEquals} myValue = '=' myValuetwo ='Enter' >=</MyButton>
+            <MyButton title= {handleClick} class={classes.Signs} myValue = '-' myValuetwo ='' >-</MyButton>
+            <MyButton title= {handleClick} class={classes.SignsPlus} myValue = '+' myValuetwo ='' >+</MyButton>
+            <MyButton answer= {result} class={classes.SignsEquals} myValue = '=' myValuetwo ='Enter' >=</MyButton>
           </div>
         </div>
       </div>
--- calculator/src/component/UI/Button/MyButton.jsx
+++ calculator/src/component/UI/Button/MyButton.jsx
@@ -3,31 +3,35 @@ import classes from './MyButton.module.css'
 
 
 function MyButton ({children, ...props}) {
-    
+    function handleClick() {
+      props.title? props.title(children):''
+      props.answer? props.answer():''
+      props.clear? props.clear():''
+      props.percent? props.percent():''
+      setButtonClicked(true)
+    }
     const [buttonClicked, setButtonClicked] = useState(false);
 
-  useEffect(() => {
     
-    function handleKeyDown(event) {
-      if (event.key === props.myValue.toString()) {
-        console.log('нажалась'+ props.myValue.toString())
-        setButtonClicked(true);
-      } else if (event.key === props.myValuetwo.toString()) {
-        console.log('нажалась'+ props.myValuetwo.toString())
-        setButtonClicked(true);
-      }
-    }
+ 
+    // useEffect(() => {
+  //   function handleKeyDown(event) {
+  //     if (event.key === props.myValue.toString() || event.key === props.myValuetwo.toString()) { 
+  //       console.log('нажалась ' + event.key);
+  //       handleClick();
+  //     }
+  //   }
 
-    window.addEventListener('keydown', handleKeyDown);
+  //   window.addEventListener('keydown', handleKeyDown);
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, []);
 
   return (
     <div>
-        <button className={props.class ? props.class.toString() : ""} onClick={() => setButtonClicked(true)}>
+        <button onClick={() => handleClick()} className={props.class ? props.class.toString() : ""} >
           {children}
         </button>
     </div>
